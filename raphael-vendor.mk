be88790d34cc56c7ab1aee8eca461a27de11f8c7
firebird11 <hbgassel@gmail.com>
2023-01-01 20:25:53 +0100

    raphael: Add Volley

--- raphael-vendor.mk
+++ raphael-vendor.mk
@@ -1308,6 +1308,7 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     camerax-vendor-extensions \
     tcmclient \
+    volley \
     com.android.hotwordenrollment.common.util \
     com.qti.dpmframework \
     dpmapi \
